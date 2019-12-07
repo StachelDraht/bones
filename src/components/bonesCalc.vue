86b6548e54b78b3c10840d9c5d30df2bdc348702
Maksim Karpenko <maksim.karpenko@gmail.com>
2019-12-07 21:53:03 +0200

    vuetify

---
+++ src/components/bonesCalc.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <div>
+            <v-text-field
+                label="Solo"
+                placeholder="Placeholder"
+                solo
+            ></v-text-field>
+        </div>
+        <input type="text" name="r" placeholder="r" v-model="r" />
+        <input type="tex" name="B" placeholder="L" v-model="l" />
+        <div>
+            Best fit circle glenoid area (A) = &#928;(r)<sup>2</sup> = {{ valueA }}
+        </div>
+        <div>
+            Area of bone loss (B) = r<sup>2</sup> / 2 (&#928; / 180 * L - sin L) = {{ valueB }}
+        </div>
+        <div>
+            Percentage bone loss: B/A * 100% = {{ valueResult }}
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data: function() {
+        return {
+            l : '',
+            r : ''
+        }
+    },
+    computed: {
+        valueA: function() {
+            return Math.PI*(Math.pow(this.r, 2))
+        },
+        valueB: function() {
+            let a = Math.pow(this.r,2) / 2
+            let sin = (Math.sin(this.l*Math.PI/180))
+            return a * (Math.PI / 180 * this.l - sin) //r2 / 2 (Π / 180 * L - sin L) 
+        },
+        valueResult: function() {
+            return (this.valueB/this.valueA) * 100
+        }
+    }
+}
+</script>
